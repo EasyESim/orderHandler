5209260911708579194b85a3772ec690c4890757
Mike Hughes <mike.p.hughes@hotmail.co.uk>
2024-05-15 21:21:48 +0100

    bumped version

--- esim_go_client.py
+++ esim_go_client.py
@@ -6,6 +6,7 @@ import zipfile
 import io
 import boto3
 
+
 class EsimGoClient:
     def __init__(self):
         self.auth_key = os.environ['ESIM_GO_AUTH_KEY']
@@ -13,8 +14,8 @@ class EsimGoClient:
         self.s3_bucket_name = 'esim-qrcode'
 
     def new_order(self, order):
-        url = 'https://api.esim-go.com/v2.2/orders'
-        
+        url = 'https://api.esim-go.com/v2.3/orders'
+
         # Initialize an empty list to store order items in the payload
         order_items_payload = []
 
@@ -34,22 +35,23 @@ class EsimGoClient:
         }
         headers = {"X-API-Key": self.auth_key}
         http = urllib3.PoolManager()
-        r = http.request('POST', url, body=json.dumps(payload), headers=headers)
+        r = http.request('POST', url, body=json.dumps(
+            payload), headers=headers)
 
         response_text = r.data.decode('utf-8')
         return response_text
 
     def get_esim_details(self, order_reference):
-        url = "https://api.esim-go.com/v2.2/esims/assignments?reference="+order_reference
-        payload={}
+        url = "https://api.esim-go.com/v2.3/esims/assignments?reference="+order_reference
+        payload = {}
         headers = {"X-API-Key": self.auth_key, 'Accept': 'application/json'}
         http = urllib3.PoolManager()
         r = http.request('GET', url, fields=payload, headers=headers)
         response_text = r.data.decode('utf-8')
         return response_text
-    
+
     def get_esim_qrcode(self, order_reference):
-        url = "https://api.esim-go.com/v2.2/esims/assignments?reference=" + order_reference
+        url = "https://api.esim-go.com/v2.3/esims/assignments?reference=" + order_reference
         payload = {}
         headers = {"X-API-Key": self.auth_key, 'Accept': 'application/zip'}
         http = urllib3.PoolManager()
@@ -68,31 +70,32 @@ class EsimGoClient:
                     if file_info.filename.lower().endswith('.png'):
                         # Read the PNG image and convert it to binary
                         with zip_file.open(file_info) as png_file:
-                            image_data = png_file.read()                           
-                        
+                            image_data = png_file.read()
+
                         # Append image name and binary data to the image_data_list
-                        
 
                          # Save the PNG image to S3
                         s3_object_key = file_info.filename
-                        self.s3_client.put_object(Bucket=self.s3_bucket_name, Key=s3_object_key, Body=image_data)
-                        
+                        self.s3_client.put_object(
+                            Bucket=self.s3_bucket_name, Key=s3_object_key, Body=image_data)
+
                         image_data_list.append({
                             'image_name': file_info.filename,
                             'image_url': 's3://'+self.s3_bucket_name+'/'+s3_object_key
                         })
-                         # Save the image locally
+                        # Save the image locally
                         # local_image_path = os.path.join(os.getcwd(), file_info.filename)
                         # with open(local_image_path, 'wb') as local_image_file:
                         #     local_image_file.write(binary_image)
 
-                        
-                        print(f"PNG image '{file_info.filename}' extracted from ZIP")
+                        print(
+                            f"PNG image '{file_info.filename}' extracted from ZIP")
                     else:
                         print(f"Ignoring file '{file_info.filename}'")
-           
+
             return image_data_list
         else:
-            print(f"Failed to download ZIP file. Status code: {response.status_code}")
+            print(
+                f"Failed to download ZIP file. Status code: {response.status_code}")
 
         return None  # Return None if the download fails
